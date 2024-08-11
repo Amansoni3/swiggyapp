42fc36666a3b2c243303b3f77197d25fece8ac22
Amansoni3 <soni.aman9039@gmail.com>
2024-08-11 17:12:41 +0530

    Removed some bugs

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
 const heading = React.createElement("h1" , {id:"heading"} , "Hello world")
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(heading)
