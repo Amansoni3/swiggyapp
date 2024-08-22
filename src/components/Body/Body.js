675e37cbeab97865ab2da3c98b1102e47fe8ed42
Amansoni3 <soni.aman9039@gmail.com>
2024-08-22 23:54:24 +0530

    Fetched swiggy api

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
 import ResturantCard from '../ResturantCard/ResturantCard'
+import { data } from '../../utlis/data'
 
 const Body = () => {
-    const data = {
-        name: "Meghna Foods",
-        dish: "Biryani, North India, Asian",
-        rating: "4.4 Stars",
-        time: "38 mins"
+    const [listOfResturants , setListOfResturants] = useState(data)
+    
+    useEffect(()=>{
+        fetchedData()
+    },[])
+
+    const fetchedData = async() => {
+        const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=26.2124007&lng=78.1772053")
+        
+        const json = await data.json()
+        console.log(json?.data)
     }
 
     return (
         <div className="body">
-            <div className="search">
-                Search
+            <div className="filter">
+                <button className='filter-btn' onClick={() => {
+                    const filteredList = listOfResturants?.filter(
+                        (res) => parseFloat(res.rating) > 4
+                    )
+                    setListOfResturants(filteredList)
+                   
+                }}
+                >
+                    Top rated resturants
+                </button>
             </div>
             <div className="res-container">
-                <ResturantCard data={data} />
+                {
+                    listOfResturants?.map((item, index) => (
+                        <ResturantCard key={index} data={item} />
+                    ))
+                }
             </div>
         </div>
     )
---
+++ src/utlis/data.js
@@ -0,0 +1,182 @@
+export const data = [
+    {
+        name: "Spice Symphony",
+        dish: "Biryani, North Indian, Asian",
+        rating: "4.5 Stars",
+        time: "30 mins"
+    },
+    {
+        name: "Curry House",
+        dish: "Butter Chicken, North Indian, Mughlai",
+        rating: "4.2 Stars",
+        time: "25 mins"
+    },
+    {
+        name: "The Tandoori Flames",
+        dish: "Tandoori Chicken, North Indian, BBQ",
+        rating: "3.6 Stars",
+        time: "35 mins"
+    },
+    {
+        name: "Naan Stop",
+        dish: "Naan, North Indian, Curry",
+        rating: "2.3 Stars",
+        time: "40 mins"
+    },
+    {
+        name: "Saffron Spice",
+        dish: "Paneer Tikka, North Indian, Vegetarian",
+        rating: "3.1 Stars",
+        time: "28 mins"
+    },
+    {
+        name: "Bombay Grill",
+        dish: "Chicken Tikka Masala, North Indian, Grill",
+        rating: "3.7 Stars",
+        time: "45 mins"
+    },
+    {
+        name: "Taste of India",
+        dish: "Dal Makhani, North Indian, Veg",
+        rating: "4.4 Stars",
+        time: "32 mins"
+    },
+    {
+        name: "Royal Rasoi",
+        dish: "Rogan Josh, North Indian, Mughlai",
+        rating: "3.5 Stars",
+        time: "38 mins"
+    },
+    {
+        name: "Masala Magic",
+        dish: "Biryani, North Indian, Asian",
+        rating: "3.2 Stars",
+        time: "36 mins"
+    },
+    {
+        name: "Flavors of India",
+        dish: "Chole Bhature, North Indian, Street Food",
+        rating: "3.3 Stars",
+        time: "42 mins"
+    },
+    {
+        name: "Desi Deli",
+        dish: "Kebabs, North Indian, BBQ",
+        rating: "4.1 Stars",
+        time: "37 mins"
+    },
+    {
+        name: "Maharaja Feast",
+        dish: "Biryani, North Indian, Mughlai",
+        rating: "4.6 Stars",
+        time: "33 mins"
+    },
+    {
+        name: "Spice Junction",
+        dish: "Paneer Butter Masala, North Indian, Veg",
+        rating: "3.4 Stars",
+        time: "31 mins"
+    },
+    {
+        name: "Indian Spice Route",
+        dish: "Samosas, North Indian, Street Food",
+        rating: "3.2 Stars",
+        time: "29 mins"
+    },
+    {
+        name: "Tandoori House",
+        dish: "Tandoori Roti, North Indian, Grill",
+        rating: "2.5 Stars",
+        time: "39 mins"
+    },
+    {
+        name: "Curry Express",
+        dish: "Butter Chicken, North Indian, Mughlai",
+        rating: "2.7 Stars",
+        time: "34 mins"
+    },
+    {
+        name: "Flavor Town",
+        dish: "Aloo Paratha, North Indian, Vegetarian",
+        rating: "2.3 Stars",
+        time: "41 mins"
+    },
+    {
+        name: "Spice Bazaar",
+        dish: "Chicken Curry, North Indian, Asian",
+        rating: "2.1 Stars",
+        time: "27 mins"
+    },
+    {
+        name: "Saffron Lounge",
+        dish: "Paneer Tikka, North Indian, BBQ",
+        rating: "4.6 Stars",
+        time: "40 mins"
+    },
+    {
+        name: "Indian Aroma",
+        dish: "Mutton Curry, North Indian, Mughlai",
+        rating: "4.4 Stars",
+        time: "35 mins"
+    },
+    {
+        name: "Masala Junction",
+        dish: "Veg Biryani, North Indian, Asian",
+        rating: "4.2 Stars",
+        time: "38 mins"
+    },
+    {
+        name: "The Curry Club",
+        dish: "Chole Kulche, North Indian, Street Food",
+        rating: "3.3 Stars",
+        time: "30 mins"
+    },
+    {
+        name: "Spice House",
+        dish: "Chicken Biryani, North Indian, Mughlai",
+        rating: "3.7 Stars",
+        time: "32 mins"
+    },
+    {
+        name: "Tandoor Treats",
+        dish: "Paneer Tikka, North Indian, Grill",
+        rating: "3.4 Stars",
+        time: "43 mins"
+    },
+    {
+        name: "Curry Leaf",
+        dish: "Rogan Josh, North Indian, Mughlai",
+        rating: "2.1 Stars",
+        time: "28 mins"
+    },
+    {
+        name: "Indian Delight",
+        dish: "Dal Tadka, North Indian, Veg",
+        rating: "4.6 Stars",
+        time: "31 mins"
+    },
+    {
+        name: "Spice Villa",
+        dish: "Butter Naan, North Indian, Curry",
+        rating: "3.2 Stars",
+        time: "37 mins"
+    },
+    {
+        name: "Taste of Punjab",
+        dish: "Amritsari Kulcha, North Indian, Street Food",
+        rating: "3.5 Stars",
+        time: "40 mins"
+    },
+    {
+        name: "Royal Curry",
+        dish: "Chicken Tikka, North Indian, Grill",
+        rating: "4.4 Stars",
+        time: "35 mins"
+    },
+    {
+        name: "Masala Corner",
+        dish: "Mutton Biryani, North Indian, Asian",
+        rating: "4.3 Stars",
+        time: "29 mins"
+    }
+];
